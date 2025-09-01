36572719ff8e6e024d99c6eec785ad17eb2b3068
Luxy <luciacosta200626@gmail.com>
2025-09-01 19:12:51 -0300

    Lucia
    Modifique el modulo fecha

--- src/fecha.cpp
+++ src/fecha.cpp
@@ -1,6 +1,7 @@
 #include "../include/fecha.h"
 
 struct rep_fecha {
+    nat dia, mes, anio;
 };
 
 TFecha crearTFecha(nat dia, nat mes, nat anio) {
@@ -9,13 +10,20 @@ TFecha crearTFecha(nat dia, nat mes, nat anio) {
 }
 
 void liberarTFecha(TFecha &fecha) {
+    delete fecha;
+    fecha = NULL;
 }
 
 void imprimirTFecha(TFecha fecha) {
+    
+    printf("%u/%u/%u\n", fecha->dia, fecha->mes, fecha->anio);
 }
 
 TFecha copiarTFecha(TFecha fecha) {
-    return NULL;
+    if (fecha == NULL)
+        return NULL; // por si no me llegan a pasar ninguna fecha
+    else
+        return crearTFecha(fecha->dia, fecha->mes, fecha->anio);
 }
 
 void aumentarTFecha(TFecha &fecha, nat dias) {
