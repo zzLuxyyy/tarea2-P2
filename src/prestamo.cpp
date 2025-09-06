8a3060d4887b3d8cbdb1326b3a266e32003784bf
anelladiulio <anelladiulio@gmail.com>
2025-09-06 14:39:42 -0300

    no me dio para hacer mucho, me base en mi coso de libro de la tarea 1 edita lo q quieras

--- src/prestamo.cpp
+++ src/prestamo.cpp
@@ -1,14 +1,32 @@
 
 #include "../include/prestamo.h"
 
+
 struct rep_prestamo {
+  Tsocio socio
+  Tlibro libro
+  Tfecha fechaDevolucion
+  Tfecha fechaRetiro
 };
 
 TPrestamo crearTPrestamo(TSocio socio, TLibro libro, TFecha fechaRetiro){
-  return NULL;
+  Tprestamo nuevoprestamo = new rep_prestamo
+  nuevoprestamo->socio = socio;
+  nuevoprestamo->libro = libro;
+  nuevoprestamo->fechaRetiro = fechaRetiro;
+  nuevoprestamo->fechaDevolucion = NULL;
+
+  return nuevoprestamo;
 }
  
 void imprimirTPrestamo(TPrestamo prestamo){
+  printf("Préstamo de libro <%s> a <%s> <%s>.", tituloTlibro(prestamo->libro), nombreTsocio(prestamo->socio), apellidoTsocio(prestamo->socio));  // 
+// <fecha retiro>
+// <fecha devolución>
+//
+// Si la fecha de devolución es NULL, en lugar de '<fecha devolución>' se imprime 'No retornado'.
+/* Requisitos específicos de la implementación solicitada: */
+// Debe ejecutar en O(1) peor caso    
 }
 
 void liberarTPrestamo(TPrestamo &prestamo){
